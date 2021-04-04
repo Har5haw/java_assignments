47e2e548610bf05d6af5fc4f5da6bf2db5c7aa89
Har5haw <harshavardhan.gadireddy@zemosolabs.com>
2021-04-04 21:59:33 +0530

    fixed some

--- src/Java_Assignment_7/Fifth/SecondClass.java
+++ src/Java_Assignment_7/Fifth/SecondClass.java
@@ -1,6 +1,6 @@
 package Java_Assignment_7.Fifth;
 
-public class SecondClass extends FirstClass{
+public class SecondClass{
     public class SecondInner extends FirstInner {
         public SecondInner(int a) {
             super(a);
